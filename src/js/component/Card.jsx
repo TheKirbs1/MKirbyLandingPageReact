c4d1a0ff4457d18020c25419b1f72de6cd7c301a
Matt Kirby <160439337+TheKirbs1@users.noreply.github.com>
2024-04-25 20:32:25 +0000

    components complete and responsive

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -2,9 +2,50 @@ import React from 'react';
 
 const Card = () => {
 	return (
-	<>
-		Card!
-	</>
+		<div className="row row-cols-1 row-cols-md-3 g-4 m-5">
+		<div className="col">
+		  <div className="card h-100">
+			<img src="https://placehold.co/500x325" className="card-img-top" alt="500x325 image placeholder" />
+			<div className="card-body">
+			  <h1 className="card-title">Card title</h1>
+			  <h4 className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec velit purus, lobortis ut risus convallis, semper interdum neque. Etiam vitae scelerisque nisi. Vestibulum porttitor </h4>
+			</div>
+			<div className="card-footer">
+				<div className="d-grid gap-2 col-6 mx-auto">
+  					<button class="btn btn-primary btn-lg" type="button"><strong>Find Out More!</strong></button>
+				</div>
+			</div>
+		  </div>
+		</div>
+		<div className="col">
+		  <div className="card h-100">
+			<img src="https://placehold.co/500x325" className="card-img-top" alt="500x325 image placeholder" />
+			<div className="card-body">
+			  <h1 className="card-title">Card title</h1>
+			  <h4 className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec velit purus, lobortis ut risus convallis, semper interdum neque. Etiam vitae scelerisque nisi. Vestibulum porttitor </h4>
+			</div>
+			<div className="card-footer">
+				<div className="d-grid gap-2 col-6 mx-auto">
+  					<button class="btn btn-primary btn-lg" type="button"><strong>Find Out More!</strong></button>
+				</div>
+			</div>
+		  </div>
+		</div>
+		<div className="col">
+		  <div className="card h-100">
+			<img src="https://placehold.co/500x325" className="card-img-top" alt="500x325 image placeholder" />
+			<div className="card-body">
+			  <h1 className="card-title">Card title</h1>
+			  <h4 className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec velit purus, lobortis ut risus convallis, semper interdum neque. Etiam vitae scelerisque nisi. Vestibulum porttitor </h4>
+			</div>
+			<div className="card-footer">
+				<div className="d-grid gap-2 col-6 mx-auto">
+					<button class="btn btn-primary btn-lg" type="button"><strong>Find Out More!</strong></button>
+				</div>
+			</div>
+		  </div>
+		</div>
+	  </div>
 	);
 };
 
--- src/js/component/Footer.jsx
+++ src/js/component/Footer.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-	<>
-		Footer!
-	</>
+		<div className="container-fluid p-0">
+			<footer className="bg-dark text-center text-white">
+				<div className="text-center" >
+    © 2015 Copyright:
+   				 <a className="text-white" href="https://4geeks.com/">4Geeks</a>
+ 			 </div>
+			</footer>
+		</div>
+
 	);
 };
 
